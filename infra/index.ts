0391605ccde1d3558c23d37435ee6a690a540ab7
Debkanchan Samadder <debu.samadder@gmail.com>
2023-08-11 21:22:43 +0530

    ci: migrate to cloudbuild

--- infra/index.ts
+++ infra/index.ts
@@ -1,61 +1,89 @@
 import * as pulumi from "@pulumi/pulumi";
 import * as gcp from "@pulumi/gcp";
 
-const imageconfig = new pulumi.Config("image");
-const serviceConfig = new pulumi.Config("service");
+const serviceName =
+  new pulumi.Config("service").get("name") || pulumi.getProject();
+const location = gcp.config.region || "asia-east1";
 
-const location = gcp.config.region || "asia-south2";
-const serviceName = serviceConfig.get("name") || pulumi.getProject();
+const github_connection = gcp.cloudbuildv2.Connection.get(
+  "github-connection",
+  pulumi.interpolate`projects/${gcp.config.project}/locations/${location}/connections/GitHub`
+);
 
-// Cloud Run
-const service = new gcp.cloudrun.Service("service", {
+const repository = new gcp.cloudbuildv2.Repository("repository", {
+  location,
+  name: serviceName,
+  parentConnection: github_connection.name,
+  remoteUri: pulumi.interpolate`https://github.com/ride-app/${serviceName}.git`,
+});
+
+new gcp.cloudbuild.Trigger("build-trigger", {
   name: serviceName,
   location,
-  template: {
-    metadata: {
-      annotations: {
-        "autoscaling.knative.dev/maxScale": "10",
-      },
-    },
-    spec: {
-      containers: [
-        {
-          image: `asia-south2-docker.pkg.dev/${
-            gcp.config.project
-          }/docker-registry/${serviceName}:${
-            imageconfig.get("tag") ?? "latest"
-          }`,
-          ports: [{ containerPort: 50051, name: "h2c" }],
-          envs: [
-            {
-              name: "FIREBASE_PROJECT_ID",
-              value: gcp.config.project,
-            },
-            {
-              name: "DEBUG",
-              value: new pulumi.Config().get("debug") ?? "false",
-            },
-          ],
-        },
-      ],
+  repositoryEventConfig: {
+    repository: repository.id,
+    push: {
+      branch: "^main$",
     },
   },
+  substitutions: {
+    _DEBUG: new pulumi.Config().get("debug") ?? "false",
+  },
+  filename: "cloudbuild.yaml",
+  includeBuildLogs: "INCLUDE_BUILD_LOGS_WITH_STATUS",
 });
 
-const policyData = gcp.organizations.getIAMPolicy({
-  bindings: [
-    {
-      role: "roles/run.invoker",
-      members: ["allUsers"],
-    },
-  ],
-});
+// const imageconfig = new pulumi.Config("image");
 
-const noauthIamPolicy = new gcp.cloudrun.IamPolicy("no-auth-iam-policy", {
-  location: service.location,
-  project: service.project,
-  service: service.name,
-  policyData: policyData.then((noauthIAMPolicy) => noauthIAMPolicy.policyData),
-});
+// // Cloud Run
+// const service = new gcp.cloudrun.Service("service", {
+//   name: serviceName,
+//   location,
+//   template: {
+//     metadata: {
+//       annotations: {
+//         "autoscaling.knative.dev/maxScale": "10",
+//       },
+//     },
+//     spec: {
+//       containers: [
+//         {
+//           image: `asia-south2-docker.pkg.dev/${
+//             gcp.config.project
+//           }/docker-registry/${serviceName}:${
+//             imageconfig.get("tag") ?? "latest"
+//           }`,
+//           ports: [{ containerPort: 50051, name: "h2c" }],
+//           envs: [
+//             {
+//               name: "FIREBASE_PROJECT_ID",
+//               value: gcp.config.project,
+//             },
+//             {
+//               name: "DEBUG",
+//               value: new pulumi.Config().get("debug") ?? "false",
+//             },
+//           ],
+//         },
+//       ],
+//     },
+//   },
+// });
 
-export const name = service.name;
+// const policyData = gcp.organizations.getIAMPolicy({
+//   bindings: [
+//     {
+//       role: "roles/run.invoker",
+//       members: ["allUsers"],
+//     },
+//   ],
+// });
+
+// const noauthIamPolicy = new gcp.cloudrun.IamPolicy("no-auth-iam-policy", {
+//   location: service.location,
+//   project: service.project,
+//   service: service.name,
+//   policyData: policyData.then((noauthIAMPolicy) => noauthIAMPolicy.policyData),
+// });
+
+// export const name = service.name;
